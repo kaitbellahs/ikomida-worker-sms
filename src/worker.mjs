e70e54a3f9ddd80c7995edd078758ef57515955a
khalid ait bellahs <kaitbellahs@gmail.com>
2022-09-01 23:51:52 -0300

    Update

--- src/worker.mjs
+++ src/worker.mjs
@@ -30,7 +30,7 @@ class SMSWorker {
     logger
 
     constructor() {
-        this.logger = Logger.getInstance(name, process.env?.ENV !== 'PROD')
+        this.logger = Logger.getInstance(name)
     }
 
     async run() {
